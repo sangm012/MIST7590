13ca3a788314d9e11943e693912faf6b1d84ac89
tjenkins06 <49869205+tjenkins06@users.noreply.github.com>
2019-11-25 17:11:34 -0500

    Update ui.R
    Updated drop down and labels

--- ui.R
+++ ui.R
@@ -33,8 +33,8 @@ shinyUI(fluidPage(shinythemes::themeSelector(),
         #                "LED Life:",
         #                value = LEDLife),
         
-       selectInput("vegetable",label = "Select Plant", c("Select Plant", Lettuce ="1",Squash ="2",Cucumber = "3"), selected = "Select Plant", selectize = TRUE),
-       selectInput("region",label = "Select Region", c("Select Region", "Athens", "Cornell"),selected = "Select Region",selectize = TRUE)
+       selectInput("vegetable",label = "Plant", c("Select Plant", Lettuce ="1",Squash ="2",Cucumber = "3"), selected = "Select Plant", selectize = TRUE),
+       selectInput("region",label = "Region", c("Select Region", "Athens", "Cornell"),selected = "Select Region",selectize = TRUE)
        
         ),
 
